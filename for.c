945c277c97824745edac6a25cbb71ec8f6ec2a06
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-25 16:01:29 +0000

    Thu, Jan 25, 2024, 11:01 PM +07:00

--- for.c
+++ for.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "meow.h"
 
 int main(void) {
     for (int i = 0; i < 5; i++) {
-        printf("*********----------\n");
+        meow1();
     }
     for (int i = 0; i < 4; i++) {
-        printf("-------------------\n");
+        meow();
     }
 }
---
+++ meow.h
@@ -0,0 +1,7 @@
+void meow(void) {
+    printf("-------------------\n");
+}
+
+void meow1(void) {
+    printf("*********----------\n");
+}
