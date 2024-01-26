1bbb1b9cf3f8ea58f6c0c5fb5ae31757ae92bc10
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-26 23:52:35 +0000

    Sat, Jan 27, 2024, 6:52 AM +07:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,23 +10,18 @@ int count_sentences(string text);
 
 int main(void)
 {
-    // Prompt the user for some text
     string text = get_string("Text: ");
 
-    // Count the number of letters, words, and sentences in the text
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
 
-    // Compute the Coleman-Liau index
     float L = (float)letters / words * 100;
     float S = (float)sentences / words * 100;
     float index = 0.0588 * L - 0.296 * S - 15.8;
 
-    // Round the result to the nearest whole number
     int grade = round(index);
 
-    // Print the grade level
     if (grade >= 16)
     {
         printf("Grade 16+\n");
