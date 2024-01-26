d86ad004871d364f93548437a497e951f51f6093
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-26 21:39:10 +0000

    Sat, Jan 27, 2024, 4:39 AM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 // Function to calculate the score of a word
-int calculateScore(char *word, int *indexToScore) {
+int calculateScore(char *word) {
     int score = 0;
 
     while (*word != '\0') {
@@ -33,19 +34,17 @@ int main() {
     int indexToScore[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     // Input words for Player 1 and Player 2
-    char word1[100], word2[100];
+    char *word1, *word2;
 
     // Prompting for Player 1's word
-    printf("Player 1, enter your word: ");
-    scanf("%s", word1);
+    word1 = GetString("Player 1, enter your word: ");
 
     // Prompting for Player 2's word
-    printf("Player 2, enter your word: ");
-    scanf("%s", word2);
+    word2 = GetString("Player 2, enter your word: ");
 
     // Calculating scores for both players
-    int score1 = calculateScore(word1, indexToScore);
-    int score2 = calculateScore(word2, indexToScore);
+    int score1 = calculateScore(word1);
+    int score2 = calculateScore(word2);
 
     // Determining the winner or if it's a tie
     if (score1 > score2) {
@@ -55,6 +54,10 @@ int main() {
     } else {
         printf("Tie!\n");
     }
+
+    // Freeing the memory allocated by GetString
+    free(word1);
+    free(word2);
 
     return 0;
 }
