1a48917cd64b403e5cc143183c4075b78aede0ca
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-26 21:52:48 +0000

    Sat, Jan 27, 2024, 4:52 AM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,20 +2,7 @@
 
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int getPoints(char word[]) {
-  int score = 0;
-  int i = 0;
-
-  while (word[i] != '\0') {
-    if (word[i] >= 'A' && word[i] <= 'Z') {
-      score += points[word[i] - 'A'];
-    }
-
-    i++;
-  }
-
-  return score;
-}
+int getPoints(char word[]);
 
 int main() {
 
@@ -39,4 +26,20 @@ int main() {
   }
 
   return 0;
+}
+
+
+int getPoints(char word[]) {
+  int score = 0;
+  int i = 0;
+
+  while (word[i] != '\0') {
+    if (word[i] >= 'A' && word[i] <= 'Z') {
+      score += points[word[i] - 'A'];
+    }
+
+    i++;
+  }
+
+  return score;
 }
