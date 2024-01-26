649715c94608751eb9dff48b1558e5e99cbbb33b
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-26 22:34:42 +0000

    Sat, Jan 27, 2024, 5:34 AM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,7 +7,8 @@ void printPlayerScore(char player[], int score);
 
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int main() {
+int main()
+{
     char word1[20], word2[20];
 
     promptForWord("Player 1", word1);
@@ -23,19 +24,22 @@ int main() {
     return 0;
 }
 
-
-int getPoints(char word[]) {
+int getPoints(char word[])
+{
     int score = 0;
     int i = 0;
 
-    while (word[i] != '\0') {
+    while (word[i] != '\0')
+    {
         char currentChar = word[i];
 
-        if (currentChar >= 'A' && currentChar <= 'Z') {
+        if (currentChar >= 'A' && currentChar <= 'Z')
+        {
             currentChar = currentChar - 'A' + 'a';
         }
 
-        if (currentChar >= 'a' && currentChar <= 'z') {
+        if (currentChar >= 'a' && currentChar <= 'z')
+        {
             score += points[currentChar - 'a'];
         }
 
@@ -45,21 +49,29 @@ int getPoints(char word[]) {
     return score;
 }
 
-void promptForWord(char player[], char word[]) {
+void promptForWord(char player[], char word[])
+{
     printf("%s, enter your word: ", player);
     scanf("%s", word);
 }
 
-void printPlayerScore(char player[], int score) {
+void printPlayerScore(char player[], int score)
+{
     printf("%s's score: %d\n", player, score);
 }
 
-void printWinnerOrTie(int score1, int score2) {
-    if (score1 > score2) {
+void printWinnerOrTie(int score1, int score2)
+{
+    if (score1 > score2)
+    {
         printf("Player 1 wins!\n");
-    } else if (score2 > score1) {
+    }
+    else if (score2 > score1)
+    {
         printf("Player 2 wins!\n");
-    } else {
+    }
+    else
+    {
         printf("Tie!\n");
     }
 }
