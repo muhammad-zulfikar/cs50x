b690568aa283c809a2a8ba2cc02d71b129e57f44
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-26 22:32:00 +0000

    Sat, Jan 27, 2024, 5:32 AM +07:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,45 +1,65 @@
 #include <stdio.h>
 
+int getPoints(char word[]);
+void promptForWord(char player[], char word[]);
+void printWinnerOrTie(int score1, int score2);
+void printPlayerScore(char player[], int score);
+
 int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int getPoints(char word[]);
-
 int main() {
+    char word1[20], word2[20];
 
-  char word1[20], word2[20];
+    promptForWord("Player 1", word1);
+    int score1 = getPoints(word1);
+    promptForWord("Player 2", word2);
+    int score2 = getPoints(word2);
 
-  printf("Player 1, enter your word: ");
-  scanf("%s", word1);
+    printPlayerScore("Player 1", score1);
+    printPlayerScore("Player 2", score2);
 
-  printf("Player 2, enter your word: ");
-  scanf("%s", word2);
+    printWinnerOrTie(score1, score2);
 
-  int score1 = getPoints(word1);
-  int score2 = getPoints(word2);
-
-  if (score1 > score2) {
-      printf("Player 1 wins!\n");
-  } else if (score2 > score1) {
-      printf("Player 2 wins!\n");
-  } else {
-      printf("Tie!\n");
-  }
-
-  return 0;
+    return 0;
 }
 
 
 int getPoints(char word[]) {
-  int score = 0;
-  int i = 0;
+    int score = 0;
+    int i = 0;
 
-  while (word[i] != '\0') {
-    if (word[i] >= 'A' && word[i] <= 'Z') {
-      score += points[word[i] - 'A'];
+    while (word[i] != '\0') {
+        char currentChar = word[i];
+
+        if (currentChar >= 'A' && currentChar <= 'Z') {
+            currentChar = currentChar - 'A' + 'a';
+        }
+
+        if (currentChar >= 'a' && currentChar <= 'z') {
+            score += points[currentChar - 'a'];
+        }
+
+        i++;
     }
 
-    i++;
-  }
+    return score;
+}
 
-  return score;
+void promptForWord(char player[], char word[]) {
+    printf("%s, enter your word: ", player);
+    scanf("%s", word);
+}
+
+void printPlayerScore(char player[], int score) {
+    printf("%s's score: %d\n", player, score);
+}
+
+void printWinnerOrTie(int score1, int score2) {
+    if (score1 > score2) {
+        printf("Player 1 wins!\n");
+    } else if (score2 > score1) {
+        printf("Player 2 wins!\n");
+    } else {
+        printf("Tie!\n");
+    }
 }
