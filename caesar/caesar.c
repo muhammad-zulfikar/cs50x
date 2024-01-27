30c85125c273fecc4c0904e92fb9dae463df21d1
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-27 03:35:15 +0000

    Sat, Jan 27, 2024, 10:35 AM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,50 +1,64 @@
+#include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-void encrypt(char *plaintext, int key);
+// Function prototypes
+bool only_digits(char *s);
+char rotate(char c, int n);
 
-int main(int argc, char argv[]) {
+int main(int argc, char *argv[]) {
+    // Make sure program was run with just one command-line argument
     if (argc != 2) {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    for (int i = 0; i < strlen(argv[1]); i++) {
-        if (!isdigit(argv[1][i])) {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
+    // Make sure every character in argv[1] is a digit
+    if (!only_digits(argv[1])) {
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
 
+    // Convert argv[1] from a string to an int
     int key = atoi(argv[1]);
 
-    char plaintext[1000];
+    // Prompt user for plaintext
     printf("plaintext: ");
-    fgets(plaintext, sizeof(plaintext), stdin);
+    char *plaintext = get_string();
 
-    encrypt(plaintext, key);
+    // Print ciphertext
+    printf("ciphertext: ");
+
+    // For each character in the plaintext:
+    for (int i = 0, n = strlen(plaintext); i < n; i++) {
+        // Rotate the character if it's a letter
+        printf("%c", rotate(plaintext[i], key));
+    }
+
+    printf("\n");
 
     return 0;
 }
 
-void encrypt(char *plaintext, int key) {
-    printf("ciphertext: ");
-
-    for (int i = 0; i < strlen(plaintext); i++) {
-        char c = plaintext[i];
-
-        if (isupper(c)) {
-            printf("%c", 'A' + (c - 'A' + key) % 26);
-        }
-        else if (islower(c)) {
-            printf("%c", 'a' + (c - 'a' + key) % 26);
-        }
-        else {
-            printf("%c", c);
+// Function to check if a string consists of only digits
+bool only_digits(char *s) {
+    for (int i = 0, n = strlen(s); i < n; i++) {
+        if (!isdigit(s[i])) {
+            return false;
         }
     }
+    return true;
+}
 
-    printf("\n");
+// Function to rotate a character by a given number of positions
+char rotate(char c, int n) {
+    if (isupper(c)) {
+        return (c - 'A' + n) % 26 + 'A';
+    } else if (islower(c)) {
+        return (c - 'a' + n) % 26 + 'a';
+    } else {
+        return c;
+    }
 }
