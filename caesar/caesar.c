999ab91aaf6a3bab73ea4c633245871866d9acb0
Muhammad Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-01-27 11:24:25 +0000

    Sat, Jan 27, 2024, 6:24 PM +07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+bool only_digits(char *s);
+char rotate(char c, int n);
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -12,12 +15,11 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    if !only_digits(argv[1])
+    if (!only_digits(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-}
 
     int key = atoi(argv[1]);
 
@@ -31,8 +33,27 @@ int main(int argc, string argv[])
     {
         printf("%c", rotate(plaintext[i], key));
     }
-
     printf("\n");
 
     return 0;
+}
+
+bool only_digits(char *s) {
+    for (int i = 0, n = strlen(s); i < n; i++) {
+        if (!isdigit(s[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Function to rotate a character by a given number of positions
+char rotate(char c, int n) {
+    if (isupper(c)) {
+        return (c - 'A' + n) % 26 + 'A';
+    } else if (islower(c)) {
+        return (c - 'a' + n) % 26 + 'a';
+    } else {
+        return c;
+    }
 }
